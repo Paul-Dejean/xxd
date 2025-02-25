ac814e4ba8770f934af4cf99acb8df36513bb6fb
Ebooth <pauldejeandev@gmail.com>
2025-02-25 23:04:07 +0100

    Added options little endian and group size

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,16 @@ use clap::Parser;
 #[command(about = "A copy of unix command line tool xxd", long_about = None)]
 pub struct Args {
     file: String,
+
+    #[arg(short = 'e')]
+    little_endian: bool,
+
+    #[arg(
+        short = 'g',
+        default_value_t = 2,
+        default_value_if("little_endian", "true", "4")
+    )]
+    group_size: u32,
 }
 
 pub fn execute_command(args: &Args) -> i32 {
@@ -18,30 +28,61 @@ pub fn execute_command(args: &Args) -> i32 {
         }
     };
 
-    let hex_dump = create_hex_dump(file);
+    let options = HexDumpOptions {
+        little_endian: args.little_endian,
+        group_size: args.group_size,
+    };
+
+    let hex_dump = create_hex_dump(file, &options);
     println!("{}", hex_dump.join("\n"));
     return 0;
 }
 
-fn create_hex_dump(file: Vec<u8>) -> Vec<String> {
+struct HexDumpOptions {
+    little_endian: bool,
+    group_size: u32,
+}
+
+fn create_hex_dump(file: Vec<u8>, options: &HexDumpOptions) -> Vec<String> {
     let blocks: Vec<&[u8]> = file.chunks(16).collect();
     let mut hex_dump: Vec<String> = Vec::new();
     for (index, block) in blocks.into_iter().enumerate() {
-        let hex_string = get_block_hex_string(block);
+        let hex_string = get_block_hex_string(block, options);
         let ascii_value = get_block_ascii_value(block);
         hex_dump.push(format!("{index:08x}: {hex_string} {ascii_value}"));
     }
     hex_dump
 }
 
-fn get_block_hex_string(block: &[u8]) -> String {
+fn get_block_hex_string(block: &[u8], options: &HexDumpOptions) -> String {
     let hex_block = block
         .iter()
         .map(|byte| byte_to_hex_string(*byte))
         .collect::<Vec<String>>();
 
-    let hex_string: Vec<String> = hex_block.chunks(2).map(|chunk| chunk.join("")).collect();
-    return hex_string.join(" ");
+    let size: usize = if options.group_size == 0 {
+        16
+    } else {
+        options.group_size as usize
+    };
+
+    if options.little_endian {
+        let hex_string: Vec<String> = hex_block
+            .chunks(size)
+            .map(|chunk| {
+                chunk
+                    .iter()
+                    .rev()
+                    .cloned()
+                    .collect::<Vec<String>>()
+                    .join("")
+            })
+            .collect();
+        return hex_string.join(" ");
+    } else {
+        let hex_string: Vec<String> = hex_block.chunks(size).map(|chunk| chunk.join("")).collect();
+        return hex_string.join(" ");
+    }
 }
 
 fn get_block_ascii_value(block: &[u8]) -> String {
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -16,3 +16,48 @@ fn test_should_print_hex_dump_for_a_file() {
             "00000000: 6865 6c6c 6f20 776f 726c 64 hello world",
         ));
 }
+
+#[test]
+fn test_should_print_little_endian_hex_dump_for_a_file() {
+    let mut tmp_file = NamedTempFile::new().expect("failed to create temp file");
+    write!(tmp_file, "hello world").expect("failed to write to temp file");
+
+    let mut cmd = Command::cargo_bin("cxxd").expect("binary exists");
+    cmd.arg(tmp_file.path())
+        .arg("-e")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "00000000: 6c6c6568 6f77206f 646c72 hello world",
+        ));
+}
+
+#[test]
+fn test_should_be_able_to_change_group_size() {
+    let mut tmp_file = NamedTempFile::new().expect("failed to create temp file");
+    write!(tmp_file, "hello world").expect("failed to write to temp file");
+
+    let mut cmd = Command::cargo_bin("cxxd").expect("binary exists");
+    cmd.arg(tmp_file.path())
+        .args(["-g", "3"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "00000000: 68656c 6c6f20 776f72 6c64 hello world",
+        ));
+}
+
+#[test]
+fn test_should_be_able_to_change_group_size_in_little_endian() {
+    let mut tmp_file = NamedTempFile::new().expect("failed to create temp file");
+    write!(tmp_file, "hello world").expect("failed to write to temp file");
+
+    let mut cmd = Command::cargo_bin("cxxd").expect("binary exists");
+    cmd.arg(tmp_file.path())
+        .args(["-e", "-g", "6"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "00000000: 206f6c6c6568 646c726f77 hello world",
+        ));
+}
