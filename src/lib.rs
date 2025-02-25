e79d662d16ddf8005dae2d6865887bd8fd356680
Ebooth <pauldejeandev@gmail.com>
2025-02-26 00:26:37 +0100

    Added support for options -c and -l

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,17 @@ pub struct Args {
 
     #[arg(
         short = 'g',
+        long = "group_size",
         default_value_t = 2,
         default_value_if("little_endian", "true", "4")
     )]
-    group_size: u32,
+    group_size: usize,
+
+    #[arg(short = 'l', long = "len")]
+    length: Option<usize>,
+
+    #[arg(short = 'c', long = "cols", default_value_t = 16)]
+    cols: usize,
 }
 
 pub fn execute_command(args: &Args) -> i32 {
@@ -31,58 +38,64 @@ pub fn execute_command(args: &Args) -> i32 {
     let options = HexDumpOptions {
         little_endian: args.little_endian,
         group_size: args.group_size,
+        cols: args.cols,
+        length: args.length,
     };
 
-    let hex_dump = create_hex_dump(file, &options);
+    let hex_dump = create_hex_dump(&file, &options);
     println!("{}", hex_dump.join("\n"));
     return 0;
 }
 
 struct HexDumpOptions {
     little_endian: bool,
-    group_size: u32,
+    group_size: usize,
+    cols: usize,
+    length: Option<usize>,
 }
 
-fn create_hex_dump(file: Vec<u8>, options: &HexDumpOptions) -> Vec<String> {
-    let blocks: Vec<&[u8]> = file.chunks(16).collect();
-    let mut hex_dump: Vec<String> = Vec::new();
-    for (index, block) in blocks.into_iter().enumerate() {
-        let hex_string = get_block_hex_string(block, options);
-        let ascii_value = get_block_ascii_value(block);
-        hex_dump.push(format!("{index:08x}: {hex_string} {ascii_value}"));
-    }
-    hex_dump
+fn create_hex_dump(file: &Vec<u8>, options: &HexDumpOptions) -> Vec<String> {
+    let data = if let Some(len) = options.length {
+        &file[..len]
+    } else {
+        &file
+    };
+
+    data.chunks(options.cols)
+        .enumerate()
+        .map(|(index, block)| {
+            let hex_string = get_block_hex_string(block, options);
+            let ascii_value = get_block_ascii_value(block);
+            format!("{index:08x}: {hex_string} {ascii_value}")
+        })
+        .collect()
 }
 
 fn get_block_hex_string(block: &[u8], options: &HexDumpOptions) -> String {
-    let hex_block = block
-        .iter()
-        .map(|byte| byte_to_hex_string(*byte))
-        .collect::<Vec<String>>();
-
-    let size: usize = if options.group_size == 0 {
+    let group_size = if options.group_size == 0 {
         16
     } else {
-        options.group_size as usize
+        options.group_size
     };
 
-    if options.little_endian {
-        let hex_string: Vec<String> = hex_block
-            .chunks(size)
-            .map(|chunk| {
-                chunk
+    block
+        .chunks(group_size)
+        .map(|group| {
+            if options.little_endian {
+                group
                     .iter()
                     .rev()
-                    .cloned()
-                    .collect::<Vec<String>>()
-                    .join("")
-            })
-            .collect();
-        return hex_string.join(" ");
-    } else {
-        let hex_string: Vec<String> = hex_block.chunks(size).map(|chunk| chunk.join("")).collect();
-        return hex_string.join(" ");
-    }
+                    .map(|&b| byte_to_hex_string(b))
+                    .collect::<String>()
+            } else {
+                group
+                    .iter()
+                    .map(|&b| byte_to_hex_string(b))
+                    .collect::<String>()
+            }
+        })
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
 fn get_block_ascii_value(block: &[u8]) -> String {
