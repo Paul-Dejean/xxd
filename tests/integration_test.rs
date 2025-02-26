44e991f77bd0892f6c28dfb3841caf22dda42410
Ebooth <pauldejeandev@gmail.com>
2025-02-26 01:49:44 +0100

    Added support for option seek

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -94,6 +94,47 @@ fn test_should_split_output_based_on_cols_option() {
     let lines: Vec<&str> = output_str.lines().collect();
 
     assert_eq!(lines[0], "00000000: 6865 6c6c hell");
-    assert_eq!(lines[1], "00000001: 6f20 776f o wo");
-    assert_eq!(lines[2], "00000002: 726c 64 rld");
+    assert_eq!(lines[1], "00000004: 6f20 776f o wo");
+    assert_eq!(lines[2], "00000008: 726c 64 rld");
+}
+
+#[test]
+fn test_should_skip_bytes_with_positive_seek_option() {
+    let mut tmp_file = NamedTempFile::new().expect("failed to create temp file");
+    write!(tmp_file, "hello world").expect("failed to write to temp file");
+
+    let mut cmd = Command::cargo_bin("cxxd").expect("binary exists");
+    cmd.arg(tmp_file.path())
+        .args(["-s", "2"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "00000002: 6c6c 6f20 776f 726c 64 llo world",
+        ));
+}
+
+#[test]
+fn test_should_skip_bytes_with_negative_seek_option() {
+    let mut tmp_file = NamedTempFile::new().expect("failed to create temp file");
+    write!(tmp_file, "hello world").expect("failed to write to temp file");
+
+    let mut cmd = Command::cargo_bin("cxxd").expect("binary exists");
+    cmd.arg(tmp_file.path())
+        .args(["-s=-5"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("00000006: 776f 726c 64 world"));
+}
+
+#[test]
+fn test_should_display_exact_content_with_seek_and_length_options() {
+    let mut tmp_file = NamedTempFile::new().expect("failed to create temp file");
+    write!(tmp_file, "hello world").expect("failed to write to temp file");
+
+    let mut cmd = Command::cargo_bin("cxxd").expect("binary exists");
+    cmd.arg(tmp_file.path())
+        .args(["-s", "2", "-l", "5"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("00000002: 6c6c 6f20 77 llo w"));
 }
